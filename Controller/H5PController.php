839b578800851ec1ff8a8d46f3b9ef29d996e319
jorisdugue <joris1904@live.fr>
2019-12-13 09:41:51 +0100

    Update H5PController.php
    WORKING

--- Controller/H5PController.php
+++ Controller/H5PController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Studit\H5PBundle\Core\H5PIntegration;
 
 /**
  * @Route("/h5p/")
