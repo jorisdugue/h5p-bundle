ca4675fc7a1d57adb38876b1590b87f1d586f32b
jorisvidéo <joris1904@live.fr>
2019-12-19 09:28:36 +0100

    - fix again symfony-h5p.js ... why no save this data ?

--- Resources/public/h5p/symfony-h5p.js
+++ Resources/public/h5p/symfony-h5p.js
@@ -10,6 +10,7 @@
             ns.apiVersion = H5PIntegration.editor.apiVersion;
             // Semantics describing what copyright information can be stored for media.
             ns.copyrightSemantics = H5PIntegration.editor.copyrightSemantics;
+            ns.metadataSemantics = H5PIntegration.editor.metadataSemantics;
             // Required styles and scripts for the editor
             ns.assets = H5PIntegration.editor.assets;
             // Required for assets
@@ -20,16 +21,16 @@
             }
 
             var h5peditor;
-            var $editor = $('#h5p-editor');
-            var $parameters = $('#h5p_parameters');
-            var $library = $('#h5p_library');
+            var $editor = $('#h5_p-editor');
+            var $parameters = $('#h5_p_parameters');
+            var $library = $('#h5_p_library');
             var library = $library.val();
 
             if (h5peditor === undefined) {
                 h5peditor = new ns.Editor(library, $parameters.val(), $editor[0]);
             }
 
-            $("form[name='h5p']").submit(function () {
+            $("form[name='h5_p']").submit(function () {
                 if (h5peditor !== undefined) {
                     var editorParameters = h5peditor.getParams();
 
