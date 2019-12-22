10ca50fd6f1a57f5df6adbfb89c7ee06f609e2cb
jorisvidéo <joris1904@live.fr>
2019-12-22 22:27:25 +0100

    - add EMBED

--- Controller/H5PInteractionController.php
+++ Controller/H5PInteractionController.php
@@ -101,7 +101,7 @@ class H5PInteractionController extends AbstractController{
             'title' => "H5P Content {$id}",
         ];
         //include the embed file (provide in h5p-core)
-        include $this->container->getParameter('kernel.root_dir').'/../vendor/h5p/h5p-core/embed.php';
+        include $this->get('kernel')->getProjectDir().'/vendor/h5p/h5p-core/embed.php';
         $response['#markup'] = ob_get_clean();
         //return nes Response HTML
         return new Response($response['#markup']);
