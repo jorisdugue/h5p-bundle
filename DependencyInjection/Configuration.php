0cb96ad7f8a1c2c609a27645a45d8a928be2a0a9
Joris Dugué <joris1904@live.fr>
2024-12-09 09:42:21 +0100

    fix(chore): resolve problem of missing declaration of file into services.yml

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -13,7 +13,7 @@ class Configuration implements ConfigurationInterface
      * version of Symfony H5P bundle
      * @return string
      */
-    const H5P_VERSION = '3.0.0';
+    const H5P_VERSION = '3.0.3';
 
     /**
      * Generates the configuration tree.
